f0d1cc4ea2b119299c1da0cb6bb22b0019c9c2ce
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-20 14:53:28 +0700

    Publish

---
+++ precache-manifest.3c8c90905940f9d2f7e7cd7f094d6b02.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2aa7f7b569fd6e9176f49dd31eaa970e",
+    "url": "/static/media/mongodb.2aa7f7b5.svg"
+  },
+  {
+    "revision": "648237cba2019d5a20da",
+    "url": "/static/css/main.b5cfc889.chunk.css"
+  },
+  {
+    "revision": "4a44fc992527ff50802a",
+    "url": "/static/js/1.4a44fc99.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/static/media/react.9a28da9f.svg"
+  },
+  {
+    "revision": "d255e75f6f38e21bc5b17f296248339c",
+    "url": "/static/media/babel.d255e75f.svg"
+  },
+  {
+    "revision": "cd849ab9d8ba06af809a4d72ac60f5a6",
+    "url": "/static/media/material-ui.cd849ab9.svg"
+  },
+  {
+    "revision": "85772647236e76b8e772c14c092a28ac",
+    "url": "/static/media/redux.85772647.svg"
+  },
+  {
+    "revision": "bfa69c4f3255d5c564c2ec600ff9885e",
+    "url": "/static/media/firebase.bfa69c4f.svg"
+  },
+  {
+    "revision": "648237cba2019d5a20da",
+    "url": "/static/js/main.648237cb.chunk.js"
+  },
+  {
+    "revision": "dd2a5a358ea682bed45bf98bc842b3ed",
+    "url": "/static/media/mysql.dd2a5a35.svg"
+  },
+  {
+    "revision": "530516030e26a09ac38473bd5d8eff0e",
+    "url": "/static/media/express.53051603.svg"
+  },
+  {
+    "revision": "9c29cb1798b50863f98b657dcd9c127d",
+    "url": "/static/media/graphql.9c29cb17.svg"
+  },
+  {
+    "revision": "535a4be75ce034e4c351611caae9b907",
+    "url": "/static/media/apollo.535a4be7.svg"
+  },
+  {
+    "revision": "eced1935fc6901d8aeeea28047db231d",
+    "url": "/static/media/webpack.eced1935.svg"
+  },
+  {
+    "revision": "e48e27f9d0eab488e665d8513578dfb5",
+    "url": "/static/media/nextjs.e48e27f9.svg"
+  },
+  {
+    "revision": "2c3c135cc8e06e14eb916df38e89b3fb",
+    "url": "/static/media/jest.2c3c135c.svg"
+  },
+  {
+    "revision": "67aec5b7e541bfddc09a52bc4226ac91",
+    "url": "/static/media/cover.67aec5b7.jpeg"
+  },
+  {
+    "revision": "a0be00490b0f5c2e6f8053c73ee4a248",
+    "url": "/index.html"
+  }
+];
